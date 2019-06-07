2c858a377059f5b314881837d6c23cb7cdba115d
Nikita Starukhin <creepydoll1@gmail.com>
2019-06-07 09:31:03 +0300

    no message

---
+++ TETRISAI/MovementResults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TETRISAI
+{
+    //Результаты движения вниз. Необходимы для упрощения работы алгоритма и детекта ситуации, когда игра проиграна. 
+    public class MovementResults
+    {
+
+        public bool Defeat = false;
+
+        public bool MoveMade = true;
+
+        public int RowsCleared = 0;
+    }
+}
